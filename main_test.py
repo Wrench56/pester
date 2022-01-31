488a2d6cb2a39a12d0977b83bafc2b3ac6454778
Wrench56 <dmarkreg@gmail.com>
2022-01-31 23:55:11 +0100

    Added test for EndReport

--- main_test.py
+++ main_test.py
@@ -1,14 +1,6 @@
 import pester
 from experiment import bar
 
-
-class XY(pester.Test):
-    def __init__(self) -> None:
-        pass
-    def hello(self):
-        assert True == True
-
-
 class FooBar(bar.Bar, pester.Test):
     def __init__(self) -> None:
         super().__init__()
@@ -20,16 +12,17 @@ class FooBar(bar.Bar, pester.Test):
     def static():
         return "Hello World"
 
-pester.main()
+#pester.run()
 
-
-@pester.wrapper
+report = pester.EndReport()
+@pester.wrapper(endreport=report)
 def test():
     """Testing an obvious thing with wrapper"""
     assert False == False
 
-
+@pester.wrapper(endreport=report)
 def x():
     """Testing an obvious thing with CLI pester tool"""
     assert False == False
 
+print(report.data)
