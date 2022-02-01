536ffcebe8bd883c37ac35742efc7d14940c58cd
Wrench56 <dmarkreg@gmail.com>
2022-02-01 23:49:15 +0100

    Tested the custom styles for class based testing & wrapper based testing

--- main_test.py
+++ main_test.py
@@ -1,32 +1,41 @@
 import pyster
 from experiment import bar
 
+
+style = pyster.Style(style_dict={
+    "meassure_time": False
+})
+
 class FooBar(bar.Bar, pyster.Test):
     def __init__(self) -> None:
         super().__init__()
     def my_test(self):
         """Testing an obvious thing with classes"""
-        assert "Hello World" == self.static()
+        assert "Hello World" == self.non_tested()
     
     @staticmethod
     def non_tested():
         return "Hello World"
 
-#pyster.run()
+pyster.run(style=style)
 
 
 report = pyster.EndReport()
-@pyster.wrapper(endreport=report)
+style = pyster.Style(style_dict={
+    "passed_message": "PASSED",
+    "failed_message": "FAILED"
+})
 
-
+@pyster.wrapper(endreport=report, style=style)
 def test():
     """Testing an obvious thing with wrapper"""
     assert False == False
 
-@pyster.wrapper(endreport=report)
+@pyster.wrapper(endreport=report, style=style)
 def x():
-    """Testing an obvious thing with CLI pyster tool"""
+    """Testing an obvious thing with wrapper"""
     assert False == False
 
 print(report.data)
+
 
