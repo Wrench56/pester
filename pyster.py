bf9b3fb26321492db2fda8280cf9bf3dba87c42b
Wrench56 <dmarkreg@gmail.com>
2022-02-01 23:34:58 +0100

    Created the class Style for custom output styling

--- pyster.py
+++ pyster.py
@@ -5,12 +5,55 @@ import types
 import typing
 import sys
 
+
+class Style():
+    DEFAULT_STYLE = {
+        "print_doc": True,
+        "meassure_time": True,
+        "passed_message": "[+] Passed",
+        "failed_message": "[-] Failed",
+        "runtime_message": "The function ran in => %s <="
+
+    }
+    def __init__(self, style_dict={}, **kwargs):
+        self.style = {}
+        self.style.update(self.DEFAULT_STYLE)
+
+        # Either use the style_dict or kwargs! BUT not both
+        if style_dict != {}:
+            self.style.update(style_dict)
+        elif kwargs != {}:
+            self.style.update(style_dict)
+    
+    def get(self, field) -> object:
+        return self.style.get(field)
+
+    def print_runtime(self, time):
+        time = round(time, 4)
+        if float(time) > 1 and float(time) < 2:
+            time = f"{time:.4f}sec"
+        elif float(time) > 2:
+            time = f"{time:.4f}secs"
+        else:
+            time = f"{time:.4f}ms"
+        print(self.get("runtime_message")%time)
+
+    def print_doc(self, str_):
+        """ Print the __doc__ of the test. (In pyster its used for describing the task)"""
+        print(str_)
+    
+    def print_passed(self):
+        print(self.get("passed_message"))
+
+    def print_failed(self):
+        print(self.get("failed_message"))
+
+
 class NonOverridable(type):
     def __new__(self, name, bases, dct):
         if bases and "_run" in dct:
             raise nomo.NonOverridableError("Overriding _run is not allowed")
         return type.__new__(self, name, bases, dct)
-
 class Test(metaclass=NonOverridable):
     def __init__(self) -> None:
         pass
@@ -27,25 +70,34 @@ class Test(metaclass=NonOverridable):
                     Report(method)
 
 class Report():
-    def __init__(self, func, endreport=None) -> None:
-        
+    def __init__(self, func, endreport=None, style=None) -> None:
+        if style != None:
+            self.style = style
+        else:
+            self.style = Style()
         self.endreport = endreport
         self.report(func)
 
     def report(self, func):
         try:
-            if func.__doc__:
-                print(func.__doc__)
+            if self.style.get("print_doc") and func.__doc__:
+                self.style.print_doc(func.__doc__)
             
             if self.endreport:
                 time = timeit.timeit(stmt = func)
                 self.endreport.add_time(func.__name__, time)
+                if self.style.get("meassure_time"):
+                    self.style.print_runtime(time) # Execution time actually
             else:
-                func()
+                if self.style.get("meassure_time"):
+                    time = timeit.timeit(stmt = func)
+                    self.style.print_runtime(time) # Execution time actually
+                else:
+                    func()
 
-            print("[+] Passed")
+            self.style.print_passed()
         except AssertionError:
-            print("[-] Failed")
+            self.style.print_failed()
 
 class EndReport():
     def __init__(self) -> None:
@@ -58,9 +110,7 @@ class EndReport():
             self.data[name] = {}
             self.data[name]["time"] = time
 
-
-
-def run():
+def run() -> None:
     # The module from where the main is called from
     module = inspect.getmodule(inspect.stack()[1][0])
     all_classes = (obj for name, obj in inspect.getmembers(sys.modules[module.__name__], inspect.isclass)
@@ -77,10 +127,10 @@ def wrapper(*args, **kwargs):
     if "endreport" in kwargs:
         endreport_obj = kwargs["endreport"]
         def inner(func):
-            Report(func, endreport=endreport_obj)
+            Report(func, endreport=endreport_obj, style=kwargs.get("style"))
         return inner
     else:
-        Report(args[0])
+        Report(args[0], style=kwargs.get("style"))
     
 
 
